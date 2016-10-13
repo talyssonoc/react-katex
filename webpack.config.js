f376540fdf9d45b65ae53c65955fdbdf7aab06c9
Talysson <talyssonoc@gmail.com>
2016-10-13 01:03:29 -0300

    Update tooling to generate build

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: {
+    'react-katex': './index.js',
+    'react-katex.min': './index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'ReactKaTeX',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: {
+    react: 'React',
+    katex: 'katex'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /(node_modules)/, loader: 'babel' }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+      compressor: { warnings: false },
+      sourceMap: false
+    })
+  ]
+};
